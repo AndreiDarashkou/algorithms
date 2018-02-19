2aff880fe4b130fbc44b455d5b7f34dd6685edca
andrei.darashkou <andrei.darashkou@itechart-group.com>
2018-02-19 18:05:31 +0300

    Init commit. Added numerical algorithms.

---
+++ src/main/java/com/study/Main.java
@@ -0,0 +1,24 @@
+package com.study;
+
+import com.study.numeric.NumericalAlgorithms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by andrei.darashkou on 2/19/2018.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> primeFactors = NumericalAlgorithms.findPrimeFactors(11);
+        String primeFactorsString = primeFactors.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        System.out.println(primeFactorsString);
+
+        System.out.println(NumericalAlgorithms.isPrimeNumber(121));
+    }
+
+}
