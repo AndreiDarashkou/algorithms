5cd9e2b67048958a405756224208a277cccd9378
andrei.darashkou <andrei.darashkou@itechart-group.com>
2018-02-26 17:22:31 +0300

    Added linked list contains method

--- src/test/java/com/study/linked_list/LinkedListTest.java
+++ src/test/java/com/study/linked_list/LinkedListTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Test cases for linked list")
 public class LinkedListTest {
@@ -28,4 +30,34 @@ public class LinkedListTest {
         assertEquals(3, list.getSize());
     }
 
+    @Test
+    @DisplayName("Linked list doesn't contain 3")
+    void testLinkedListDoesntContain3() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(4);
+        list.addFirst(5);
+        list.addFirst(6);
+        assertFalse(list.contains(3));
+    }
+
+    @Test
+    @DisplayName("Linked list contains 10")
+    void testLinkedListContains10() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(10);
+        list.addFirst(1);
+        list.addFirst(14);
+        assertTrue(list.contains(10));
+    }
+
+    @Test
+    @DisplayName("Linked list contains null")
+    void testLinkedListContainsNull() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(10);
+        list.addFirst(1);
+        list.addFirst(14);
+        list.addFirst(null);
+        assertTrue(list.contains(null));
+    }
 }
