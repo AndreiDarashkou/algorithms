3d3874268e8dc379a012d0a12afc1aec7f7edb82
AndreiDarashkou <andrei.darashkou@gmail.com>
2022-05-19 12:29:58 +0200

    remove trash

--- src/main/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test extends ClassLoader {
-
-    public static void main(String[] args) throws Throwable {
-        Yossarian yossarian = Catch22.loophole();
-        System.out.println(yossarian.isCrazy());
-    }
-}
--- src/main/java/Yossarian.java
+++
@@ -1,7 +0,0 @@
-public abstract class Yossarian {
-
-    public final boolean isCrazy() {
-        return true;
-    }
-
-}
